4f1e926dbb682ddda46a3bcf8ef83e868076e340
Neurotrier <neurotrier123@gmail.com>
2024-01-09 10:19:55 +0300

    add not_main function

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ def main():
     print(f"{3 + 4 = }")
 
 
+def not_main(number: str):
+    if number.isdigit():
+        print("Is not a canonic number!") if int(number) != 42 else print("Well done!")
+    else:
+        print(f"I wanted a number, not {type(number).__name__}")
+
+
+
 if __name__ == "__main__":
     main()
+    not_main(input("Enter a number: "))
