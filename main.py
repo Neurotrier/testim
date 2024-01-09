ac9ffbfbd0424a2bc587fe1ad17a627c191396ff
Neurotrier <neurotrier123@gmail.com>
2024-01-09 10:42:24 +0300

    checkout back to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
     print("Hello world!")
     print(f"{3 + 4 = }")
+    print("Its main again!")
 
 
 def not_main(number: str):
+    print("Hi there!")
     if number.isdigit():
         print("Is not a canonic number!") if int(number) != 42 else print("Well done!")
     else:
